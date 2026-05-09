98d3d0102dce3ba2279ba917d914309c19492db5
njuedominic <njuemugodominic@gmail.com>
2026-05-09 22:24:20 +0300

    Add loop to flatten nested columns

--- main.py
+++ main.py
@@ -9,11 +9,38 @@ def json_to_csv(url):
     # 1. Fetch the data
     response = requests.get(url)
     data = response.json()
-    # 2. Convert the data into a dataframe
-    df = pd.DataFrame(data)
-    # 3. Convert the dataframe into a csv and save it
-    df.to_csv("./data/qatar_airways_data.csv", index = False)
+
+    # 2. Extract the list from the dictionary
+    if isinstance(data, dict):
+        main_data = next(
+            (v for v in data.values() if isinstance(v, list)),
+            [data]
+            )
+    else:
+        main_data = data
+        
+    # 3. Normalize the data (Flattening semi-structured data)
+    df = pd.json_normalize(main_data)
+
+    # 4.Flatten nested column
+    for col in df.columns:
+        # Explode lists
+        if any(isinstance(val, list) for val in df[col].dropna()):
+            df = df.explode(col)
+
+        # Normalize dictionaries
+        if any(isinstance(val, dict) for val in df[col].dropna()):
+
+            nested_df = pd.json_normalize(df[col]).set_index(df.index)
+
+            df = (
+                df.drop(columns=[col])
+                  .join(nested_df, rsuffix=f"_{col}")
+            )
+    # Save the file
+    df.to_csv("./data/converted_data.csv", index = False)
     return df
+
 
 data_url = input("Enter url with the json data: ")
 
