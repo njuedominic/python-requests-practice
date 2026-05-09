3eccb0eeff62fb5beaf44ba754d997207a537aba
njuedominic <njuemugodominic@gmail.com>
2026-05-09 20:27:11 +0300

    Add print statement to confirm file has been saved successfully

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ import requests
 # Create a function that takes in the url with the jsoin data and returns a converted csv file
 
 def json_to_csv(url):
+    # 1. Fetch the data
     response = requests.get(url)
     data = response.json()
+    # 2. Convert the data into a dataframe
     df = pd.DataFrame(data)
-    df_to_csv = df_to_csv("../data/qatar_airways_data.csv")
-    return df_to_csv
+    # 3. Convert the dataframe into a csv and save it
+    df.to_csv("./data/qatar_airways_data.csv", index = False)
+    return df
 
+data_url = input("Enter url with the json data: ")
+
+# Call the function on the url
+extracted_df = json_to_csv(data_url)
+print("File has been saved!")
+print(extracted_df.head())
